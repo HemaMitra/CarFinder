7d610aee3bc7f8d8b685f36411475c63c3e0a555
Hema Mitra <hemakolte@hotmail.com>
2015-11-12 21:00:13 -0500

    nov12_2015
    with stored procedures

---
+++ CarFinder/Controllers/WebApiController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using CarFinder.Models;
+using CarFinder.Providers;
+using CarFinder.Results;
+using System.Threading.Tasks;
+using Bing;
+using Newtonsoft.Json;
+using System.Configuration;   
+
+namespace CarFinder.Controllers
+{
+    [RoutePrefix("api/WebApi")]
+    public class WebApiController : ApiController
+    {   
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        // Get AllYears
+        [HttpGet]
+        [Route("AllYears")]
+        public async Task<IHttpActionResult> AllYears()
+        {
+            return Ok(await db.AllYears());
+        }
+
+        // Get Makes By Year
+        [HttpGet]
+        [Route("MakesByYear")]
+        public async Task<IHttpActionResult> MakesByYear(string year)
+        {
+            return Ok(await db.MakesByYear(year));
+        }
+
+        // Get Models By Year and Make
+        [HttpGet]
+        [Route("ModelsByYearMake")]
+        public async Task<IHttpActionResult> ModelsByYearMake(string year, string make)
+        {
+            return Ok(await db.ModelsByYearMake(year,make));
+        }
+
+        // Get Trims by Year, Make and Model
+        [HttpGet]
+        [Route("Trims")]
+        public async Task<IHttpActionResult> Trims(string year, string make, string model)
+        {
+            return Ok(await db.Trims(year, make, model));
+        }
+
+        // Get Cars By Year
+        [HttpGet]
+        [Route("CarsByYear")]
+        public async Task<IHttpActionResult> CarsByYear(string year)
+        {
+            return Ok(await db.CarsByYear(year));
+        }
+
+        // Get Cars by Year and Make
+        [HttpGet]
+        [Route("CarsByYearMake")]
+        public async Task<IHttpActionResult> CarsByYearMake(string year, string make)
+        {
+            return Ok(await db.CarsByYearMake(year,make));
+        }
+
+        // Get Cars by Year, Make and Model
+        [HttpGet]
+        [Route("CarsYearMakeModel")]
+        public async Task<IHttpActionResult> CarsYearMakeModel(string year, string make, string model)
+        {
+            return Ok(await db.CarsYearMakeModel(year, make, model));
+        }
+
+        // Get Cars by Year, Make, Model and Trim
+        [HttpGet]
+        [Route("CarsYearMakeModelTrim")]
+        public async Task<IHttpActionResult> CarsYearMakeModelTrim(string year, string make, string model, string trim)
+        {
+            return Ok(await db.CarsYearMakeModelTrim(year,make,model,trim));
+        }
+
+        [HttpGet]
+        [Route("GetSearchCar")]
+        public async Task<IHttpActionResult> GetSearchCar(string year, string make, string model, string trim, string filter, bool? paging)
+        {
+            return Ok(await db.GetSearchCar(year, make, model, trim, filter, paging));
+        }
+
+        [Route("GetCar")]
+        public async Task<IHttpActionResult> GetCar(int id)
+        {
+            // Getting car images through Bing Search 
+            var car = db.Cars.Find(id);
+            if (car == null)
+                return await Task.FromResult(NotFound());
+
+            HttpResponseMessage response;
+            // Get Credentials for Bing from Web Config File
+            string searchApiServiceRootUri = ConfigurationManager.AppSettings["searchApiServiceRootUri"];
+            string accountKeyPass = ConfigurationManager.AppSettings["searchApiAccountKey"];
+
+            var client = new BingSearchContainer(new Uri(searchApiServiceRootUri));
+            client.Credentials = new NetworkCredential("accountKey", accountKeyPass);
+            
+            var marketData = client.Composite(
+                "image",
+                car.model_year + ' ' + car.make + ' ' + car.model_name + ' ' + car.model_trim,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+                ).Execute();
+
+            var result = marketData.FirstOrDefault();
+            var image = result != null ? result.Image : null;
+            var firstImage = image != null ? image.FirstOrDefault() : null;
+            var imgUrl = firstImage != null ? firstImage.MediaUrl : null;
+
+            // Getting recalls from NHTSA
+            dynamic recalls;
+            string recallBaseAddress = ConfigurationManager.AppSettings["recallBaseAddress"];
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(recallBaseAddress);
+
+                try
+                {
+                    response = await httpClient.GetAsync("webapi/api/Recalls/vehicle/modelyear/"+car.model_year+"/make/"
+                        +car.make+"/model/"+car.model_name +"? FormatException=json");
+                    
+                    // JSON returns a string so deserialize it before sending it
+                    recalls = JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
+                }
+                catch (Exception e)
+                {
+                    return InternalServerError(e);
+                }
+            }
+
+            return Ok(new { car, imgUrl, recalls });
+        }
+    }
+}
---
+++ CarFinder/Models/IdentityModels.cs
@@ -0,0 +1,156 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace CarFinder.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        // Get All Years
+        public async Task<List<string>> AllYears()
+        {
+            return await this.Database.SqlQuery<string>("AllYears").ToListAsync();
+        }
+
+        // Get Makes By Year
+
+        public async Task<List<string>> MakesByYear(string year)
+        {
+            var yearParm = new SqlParameter("@year", year); 
+            return await this.Database.SqlQuery<string>("MakesByYear @year",yearParm).ToListAsync();
+        }
+
+        // Get Models By Year And Make
+
+        public async Task<List<string>> ModelsByYearMake(string year, string make)
+        { 
+            var yearParm = new SqlParameter("@year",year);
+            var makeParm = new SqlParameter("@make",make);
+            return await this.Database.SqlQuery<string>("ModelsByYearMake @year,@make", yearParm, makeParm).ToListAsync();
+        }
+
+        // Get Trims By Year, Make and Model
+        public async Task<List<string>> Trims(string year, string make, string model)
+        {
+            var yearParm = new SqlParameter("@year", year);
+            var makeParm = new SqlParameter("@make", make);
+            var modelParm = new SqlParameter("@model",model);
+            return await this.Database.SqlQuery<string>("Trims @year,@make,@model", yearParm, makeParm,modelParm).ToListAsync();
+        }
+
+        // Get Cars By Year
+        public async Task<List<Car>> CarsByYear(string year)
+        {
+            var yearParm = new SqlParameter("@year",year);
+
+            return await this.Database.SqlQuery<Car>("CarsByYear @year",yearParm).ToListAsync();
+        }
+
+        // Get Cars By Year and Make
+
+        public async Task<List<Car>> CarsByYearMake(string year, string make)
+        {
+            var yearParm = new SqlParameter("@year",year);
+            var makeParm = new SqlParameter("@make", make);
+
+            return await this.Database.SqlQuery<Car>("CarsByYearMake @year,@make", yearParm, makeParm).ToListAsync();
+        }
+
+        // Get Cars By Year , Make and Model
+
+        public async Task<List<Car>> CarsYearMakeModel(string year, string make, string model)
+        {
+            var yearParm = new SqlParameter("@year",year);
+            var makeParm = new SqlParameter("@make", make);
+            var modelParm = new SqlParameter("@model",model);
+
+            return await this.Database.SqlQuery<Car>
+                ("CarsYearMakeModel @year,@make,@model", yearParm, makeParm, modelParm).ToListAsync();
+        }
+
+        // Get Cars by Year, Make, Model and Trim
+        public async Task<List<Car>> CarsYearMakeModelTrim(string year, string make, string model, string trim)
+        {
+            var yearParm = new SqlParameter("@year", year);
+            var makeParm = new SqlParameter("@make", make);           
+            var trimParm = new SqlParameter("@trim",trim);
+            var modelParm = new SqlParameter("@model", model);
+
+            return await this.Database.SqlQuery<Car>
+                ("CarsYearMakeModelTrim @year,@make,@model,@trim",yearParm,makeParm,modelParm,trimParm).ToListAsync();
+
+        }
+
+        // Get Cars with paging and search
+        public async Task<List<Car>> GetSearchCar(string year,string make, string model, string trim, string filter, bool? paging)
+        {
+            //var yearParm = new SqlParameter("@year", year);
+            //var makeParm = new SqlParameter("@make", make ?? "");
+            //var modelParm = new SqlParameter("@model", model ?? "");
+            //var trimParm = new SqlParameter("@trim", trim ?? "");
+            //var filterParm = new SqlParameter("@filter", filter ?? "");
+            //var pagingParm = new SqlParameter("@paging", paging ?? false);
+
+            var yearParm = new SqlParameter("@year", year);
+            var makeParm = new SqlParameter("@make", make);
+            var modelParm = new SqlParameter("@model", model);
+            var trimParm = new SqlParameter("@trim", trim);
+            var filterParm = new SqlParameter("@filter", filter ?? "");
+            var pagingParm = new SqlParameter("@paging", paging);
+
+
+            var filtered = await this.Database.SqlQuery<Car>
+                ("GetSearchCar @year,@make,@model,@trim,@filter,@paging", yearParm, makeParm, modelParm, trimParm, filterParm, pagingParm).ToListAsync();
+            return filtered;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
